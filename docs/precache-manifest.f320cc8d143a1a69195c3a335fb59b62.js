8a0ab158352b446651b6c7074bd7ed8170808514
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-23 02:01:47 +0300

    creating a new build

---
+++ docs/precache-manifest.f320cc8d143a1a69195c3a335fb59b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd676e4bc357dbd8108d7290ab2aee9",
+    "url": "/static/media/logo.9dd676e4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b08526f27833848b095c",
+    "url": "/static/js/main.b08526f2.chunk.js"
+  },
+  {
+    "revision": "1dcdb82441d572e42aab",
+    "url": "/static/js/1.1dcdb824.chunk.js"
+  },
+  {
+    "revision": "b08526f27833848b095c",
+    "url": "/static/css/main.10c3044b.chunk.css"
+  },
+  {
+    "revision": "35f335b3ccb3f13af41a37fc26631a99",
+    "url": "/index.html"
+  }
+];
