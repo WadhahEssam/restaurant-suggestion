7f4a3665f130d6518f1fad04320c3bc98b421c21
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-23 00:31:58 +0300

    extracting the welcome menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import WelcomeMenu from './WelcomeMenu';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="welcomeMenu">
-          <h1/>
-        </div>
+        <WelcomeMenu/>
       </div>
     );
   }
---
+++ src/WelcomeMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+class WelcomeMenu extends Component {
+  render() {
+    return(
+      <div className="welcomeMenu">
+        <h1/>
+      </div>
+    );
+  }
+}
+
+export default WelcomeMenu;
