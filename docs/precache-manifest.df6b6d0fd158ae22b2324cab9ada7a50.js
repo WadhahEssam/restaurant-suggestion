09a9ed18cc7c27a13ad84e0a69d5cd719f66b869
Wadah Esam <wadah-wleed@hotmail.com>
2019-01-23 08:41:12 +0300

    creating a new build

---
+++ docs/precache-manifest.df6b6d0fd158ae22b2324cab9ada7a50.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc779e7870e9742053c144546731f11",
+    "url": "./static/media/success-icon.5fc779e7.svg"
+  },
+  {
+    "revision": "362bded08cee4e3cec510719e87fa948",
+    "url": "./static/media/play-store-icon.362bded0.svg"
+  },
+  {
+    "revision": "9dd676e4bc357dbd8108d7290ab2aee9",
+    "url": "./static/media/logo.9dd676e4.png"
+  },
+  {
+    "revision": "a6fd5ae2108cdfb7133a3bf586850061",
+    "url": "./static/media/loading-icon.a6fd5ae2.svg"
+  },
+  {
+    "revision": "08e7f704103f4b9c1e774eb7b22996f0",
+    "url": "./static/media/app-store-icon.08e7f704.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "eebddf5541d91c34de6e",
+    "url": "./static/js/main.eebddf55.chunk.js"
+  },
+  {
+    "revision": "89635d1bf107b7e43ade",
+    "url": "./static/js/1.89635d1b.chunk.js"
+  },
+  {
+    "revision": "eebddf5541d91c34de6e",
+    "url": "./static/css/main.dd9173ca.chunk.css"
+  },
+  {
+    "revision": "a46e56c7a070d203adc8e451f4858344",
+    "url": "./index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.79556e0209ce2006371e7976c549bd65.js"
+  "./precache-manifest.df6b6d0fd158ae22b2324cab9ada7a50.js"
 );
 
 workbox.clientsClaim();
