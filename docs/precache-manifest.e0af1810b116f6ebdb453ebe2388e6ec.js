fd151f95d7c7b156d2a2b3aee0a6c1ee62b2f0be
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-25 19:02:36 +0300

    creating a new build

---
+++ docs/precache-manifest.e0af1810b116f6ebdb453ebe2388e6ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7dfa1e73f064c4e6ae74ba08ac8ae2e",
+    "url": "./static/media/play-store-icon.f7dfa1e7.svg"
+  },
+  {
+    "revision": "64d0872da6d2bbae5e36",
+    "url": "./static/css/main.75a86ab1.chunk.css"
+  },
+  {
+    "revision": "9511098db4dee1fef138",
+    "url": "./static/js/1.9511098d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a6fd5ae2108cdfb7133a3bf586850061",
+    "url": "./static/media/loading-icon.a6fd5ae2.svg"
+  },
+  {
+    "revision": "9dd676e4bc357dbd8108d7290ab2aee9",
+    "url": "./static/media/logo.9dd676e4.png"
+  },
+  {
+    "revision": "64d0872da6d2bbae5e36",
+    "url": "./static/js/main.64d0872d.chunk.js"
+  },
+  {
+    "revision": "9cc2a2782ac01df44b4c68c942210b13",
+    "url": "./static/media/app-store-icon.9cc2a278.svg"
+  },
+  {
+    "revision": "5fc779e7870e9742053c144546731f11",
+    "url": "./static/media/success-icon.5fc779e7.svg"
+  },
+  {
+    "revision": "510c7db72d064ffd533b64f82a24fabe",
+    "url": "./static/media/logo2.510c7db7.png"
+  },
+  {
+    "revision": "bb73c2e5cb333e3b8cbf109fe306d8d3",
+    "url": "./static/media/map-pin.bb73c2e5.png"
+  },
+  {
+    "revision": "dbbce728490d7c4cd9f0bc2f32bdbdc7",
+    "url": "./index.html"
+  }
+];
