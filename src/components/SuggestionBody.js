810defa192c13121974e64f36bb0ae8ef1bd53cc
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-25 04:31:50 +0300

    adding the google maps button and foursquare button + chaning the tab title and logo

--- src/components/SuggestionBody.js
+++ src/components/SuggestionBody.js
@@ -5,6 +5,8 @@ import RestaurantBasicInformation from '../components/ResturantBasicInformation'
 import '../css/SuggestionBody.css';
 import logo from '../img/logo2.png';
 import mapPin from '../img/map-pin.png';
+import foursquareLogo from '../img/foursquare-logo.png';
+import googleMapsLogo from '../img/google-maps-logo.png';
 
 class SuggestionBody extends Component {
   state = {
@@ -48,19 +50,29 @@ class SuggestionBody extends Component {
                 />
               </div>
               <RestaurantBasicInformation restaurant={this.props.restaurant}/>
+              <div className="links-div">
+                <a href="google.com">                
+                  <img src={googleMapsLogo} className="google-maps-logo link-logo"/>
+                </a>
+                <a href="foursquare.com">
+                  <img src={foursquareLogo} className="foursquare-logo link-logo"/>
+                </a>
+                <div className="box-shadow-div-left" />
+                <div className="box-shadow-div-right" />
+              </div>
               <div className="map-div">
-              <MapGL
-                {...this.state.viewport}
-                mapboxApiAccessToken = "pk.eyJ1Ijoid2FkYWhlc2FtMjEiLCJhIjoiY2pyODJhMDhjMDI2ZTQzb2RkNjgxMHY0diJ9.W9cODURNmVYUekoS1b2LkQ"
-                onViewportChange={(viewport) => this.setState({viewport})}
-              >
-                <Marker latitude={this.state.currentPin.latitude} longitude={this.state.currentPin.longitude}>
-                  <img 
-                    className="map-pin" 
-                    src={mapPin}
-                  />
-                </Marker>
-              </MapGL>
+                <MapGL
+                  {...this.state.viewport}
+                  mapboxApiAccessToken = "pk.eyJ1Ijoid2FkYWhlc2FtMjEiLCJhIjoiY2pyODJhMDhjMDI2ZTQzb2RkNjgxMHY0diJ9.W9cODURNmVYUekoS1b2LkQ"
+                  onViewportChange={(viewport) => this.setState({viewport})}
+                >
+                  <Marker latitude={this.state.currentPin.latitude} longitude={this.state.currentPin.longitude}>
+                    <img 
+                      className="map-pin" 
+                      src={mapPin}
+                    />
+                  </Marker>
+                </MapGL>
               </div>
             </div>
           </div>
